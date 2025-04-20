d1014d960262dd28262e06a87df41341837e2820
Suvam Naskar <suvamnaskar780@gmail.com>
2025-04-20 11:07:54 +0530

    PROD: re-Release #2

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
